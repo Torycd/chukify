6c859c3dd159582ff6d3f9b218efb8cb63f409ab
Victory Idundun <vicidundun@gmail.com>
2026-05-09 02:12:00 +0100

    commit to side

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
+    <div className="mx-16 my-3.5">
       <Navbar />
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,8 @@
 function Navbar() {
   return (
-    <div className="flex justify-between mx-11.25 my-5">
+    <div className="flex justify-between">
       <h1 className="text-3xl font-bold">Chunkify</h1>
-      <ul className="flex justify-between gap-10">
+      <ul className="flex justify-between text-xl gap-10">
         <li>Home</li>
         <li>Contact</li>
       </ul>
