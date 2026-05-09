456d57801e1fbf3cf857d648ea612d274d9c4aae
Victory Idundun <vicidundun@gmail.com>
2026-05-09 01:16:52 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar() {
+  return (
+    <div className="flex justify-between">
+      <h1>Chunkify</h1>
+      <ul className="flex justify-between">
+        <li>Home</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
