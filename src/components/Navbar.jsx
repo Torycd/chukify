6e8ea3a8fb627e226b97d88ba63460b98619f18a
Victory Idundun <vicidundun@gmail.com>
2026-05-09 08:51:50 +0100

    Navbar and Main created. Main is currently empty, but Navbar has the basic structure and styling.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ function Navbar() {
   return (
     <div className="flex justify-between">
       <h1 className="text-3xl font-bold">Chunkify</h1>
-      <ul className="flex justify-between text-xl gap-10">
+      <ul className="flex justify-between text-xl gap-10 font-semibold">
         <li>Home</li>
         <li>Contact</li>
       </ul>
